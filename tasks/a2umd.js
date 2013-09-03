f6bcd8d0666ca04a6bc8200bb7d5b4cd5ad7b560
rikkertkoppes <rikkert.koppes@theapsgroup.nl>
2013-09-03 12:13:05 +0200

    initial commit

---
+++ tasks/a2umd.js
@@ -0,0 +1,55 @@
+var esprima = require('esprima');
+var assert = require('assert');
+var handlebars = require('handlebars');
+
+function parse(src,globalName) {
+    var tree = esprima.parse(src,{
+        loc: true,
+        range: true
+    });
+    assert(tree.body.length == 1,'expected only one statement (define)');
+    assert(tree.body[0].expression.callee.name == 'define','expected "define" as first and only call');
+
+    var args = tree.body[0].expression.arguments;
+    assert(args.length>=2,'expected at least 2 arguments in define');
+    assert(args.length<=3,'expected at most 3 arguments in define');
+
+    var amd = args[args.length-3];
+    var deps = args[args.length-2];
+    var factory = args[args.length-1];
+
+    assert(deps.type=='ArrayExpression','expected an array of dependencies');
+    assert(factory.type=='FunctionExpression','expected a factory function');
+
+    var dependencyPaths = deps.elements.map(function(d) {
+        return "'"+d.value+"'";
+    });
+    var dependencyNames = factory.params.map(function(d) {
+        return 'root.'+d.name;
+    });
+    globalName = globalName || (factory.id && factory.id.name);
+    var amdName = amd && amd.value;
+
+    assert(globalName,'expected factory to have a name or global alias given as second argument');
+
+
+    var factorySrc = src.substring(factory.range[0],factory.range[1]);
+
+    return {
+        dependencyPaths: dependencyPaths,
+        dependencyNames: dependencyNames,
+        globalName: globalName,
+        amdName: amdName,
+        factorySrc: factorySrc
+    };
+}
+
+function wrap(context,template) {
+    var tpl = handlebars.compile(template);
+    return tpl(context);
+}
+
+module.exports = {
+    parse: parse,
+    wrap: wrap
+};
---
+++ tasks/grunt-a2umd.js
@@ -0,0 +1,32 @@
+var a2umd = require('./a2umd.js');
+var fs = require('fs');
+var path = require('path');
+
+module.exports = function(grunt) {
+    grunt.registerMultiTask('a2umd', 'wraps an amd module to also behave in global scope using esprima', function() {
+        var template = this.data.template||path.join(__dirname, '..', 'templates', 'wrapper.hbs');
+
+        this.files.forEach(function(f) {
+            var src = f.src.filter(function(filepath) {
+                if (!grunt.file.exists(filepath)) {
+                    grunt.log.warn('Source file "' + filepath + '" not found.');
+                    return false;
+                } else {
+                    return true;
+                }
+            });
+
+            src.forEach(function(fn) {
+                try {
+                    var src = grunt.file.read(fn);
+                    var res = a2umd.parse(src);
+                    var tpl = fs.readFileSync(template,'utf8');
+                    res = a2umd.wrap(res,tpl);
+                    grunt.file.write(f.dest,res);
+                } catch (e) {
+                    grunt.warn(fn + ': ' + e.message, 3);
+                }
+            });
+        });
+    });
+};
